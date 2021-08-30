f0d8e7b9eb3cbd10cd3006e2525eba61507bc04f
Mohammed Hussian <mohussain237@gmail.com>
2021-08-30 12:00:56 +0100

    creating asynch funtion for jokes api

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 console.log("heellloooww");
+
+
+let products = async ()=> {
+    const response= await axios.get("http//:api/google.com/jokes")
+    const data =  await response.json ;
+    return data;
+}
+
+console.log(products);
